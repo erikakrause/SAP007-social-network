c20d7477e7c4e9b5ba505d26ad8d9da50488c745
Erika Moreno <erika.kmoreno@gmail.com>
2022-05-04 11:24:26 -0300

    Updates

--- pages/feed.js
+++ pages/feed.js
@@ -1,9 +1,8 @@
 import '../firebase/config-firebase.js';
 import { logout } from '../firebase/authetication.js';
 import { createPost } from '../firebase/firestore.js';
-import { getPost } from '../firebase/firestore.js';
+import { getPost, getCurrentUser } from '../firebase/firestore.js';
 export default function feed() {
-
   const feed = document.createElement('div');
   const templateFeed = `
   <nav class="top-nav">
@@ -12,60 +11,73 @@ export default function feed() {
       </picture>
       <picture>
         <a href="#home" id="logout">
-            <img  class="button-logout" src="./img/botao-voltar.png" alt="Botão Sair">
+            <img  class="button-logout" src="./img/logout.png" alt="Botão Sair">
         </a>
       </picture>
   </nav>
   <div class= "line-header"> </div>
   <section class="publish" id="publish">
-    <textarea id="post-text" class="post-area-text" placeholder="O que você quer compartilhar?" cols="33" rows="5"></textarea>
-    <div class ="buttons" id='selected-theme'>
-      <select id='theme'>
-        <option value disabled selected>Assunto</option>
-        <option value="Destinos">Destinos</option>
-        <option value="Dicas">Dicas</option>
-        <option value="Milhas">Milhas</option>
-      </select>
-    </div>
-    <div>
-      <button id="publish-btn">Enviar</button>
-    </div>
+    <textarea id="post-text" class="post-area-text" placeholder="O que você quer compartilhar?" cols="35" rows="7"></textarea>
+        <p id="alert-notification" class="error"></p>
+        <div class= "btn-container">
+          <button class="publish-btn" id="publish-btn">Publicar</button>
+        </div>
   </section>
   <div id='posts-container' class="posts-container">  
   </div>
   `;
 
   feed.innerHTML = templateFeed;
+
   const posts = feed.querySelector('#posts-container');
   const btnPosts = feed.querySelector('#publish-btn');
   const postText = feed.querySelector('#post-text');
+  const msgAlert = feed.querySelector('#alert-notification');
+  const convertDateObject = (dateObject) => {
+    const date = dateObject.toDate();
+    return date.toLocaleString('pt-br');
+  };
+
+  const convertTime = (timestamp) => {
+    const dateObject = new Date(timestamp);
+    const humanDateFormat = dateObject.toLocaleString();
+    return humanDateFormat;
+  };
+
   btnPosts.addEventListener('click', async () => {
-    const docRef = await createPost(postText.value, "teste@teste.com");    
+    if (postText.value === '') {
+      msgAlert.innerHTML = 'Digite uma mensagem!';
+    } else;
+    await createPost(postText.value);
     posts.innerHTML += `
-    <div class= "posts w-100" id= "posts" >  
-    <p> ${postText.value} </p>    
+    <div class= "posts w-100" id= "posts" > 
+    <p> ${getCurrentUser()}</p>
+    <p>${convertTime(Date.now())}</p> 
+    <p> ${postText.value} </p> 
     </div>
     `;
   });
 
-  const convertTimestamp = (timestamp) => {
-    const date = timestamp.toDate();
-    return date.toLocaleString('pt-br');
-  };
-
   const getPostsFromDatabase = async () => {
-    const posts = await getPost();
-    posts.forEach((post) => {
+    const posts2 = await getPost();
+    posts2.forEach((post) => {
       document.querySelector('#posts-container').innerHTML += `         
-      <div class= "posts" id= "posts" >
-        <ul class="posts" id="posts">
+      <div class= "box-posts" id= "posts" >
+        <ul class="box-posts" id="posts">
           <li>
-           <p>${post.userEmail}</p> 
-           <p>${convertTimestamp(post.date)}</p>           
-           <p>${post.textPost}</p>
+          <p>${post.userName}</p> 
+          <p>${convertDateObject(post.date)}</p> 
+          <p>${post.textPost}</p>
+          <p>${post.userName}</p>
+          <div class= "line"></div>
+          <div class="icons">
+            <button class="button-heart">
+              <img class="heart-btn" id="heart-btn" src="./img/heart.svg">
+            </button>
+          </div>
           </li>
-        </ul>          
-      </div>     
+        </ul>
+      </div>  
       `;
     });
   };
